c37e4125c7ee5c8ebf4a61e6c400cdcc0b409a66
Angrey Shoful <shoful.a@gmail.com>
2020-12-05 20:21:13 +0200

    Updates

---
+++ precache-manifest.fa8c397f9cbec4faf2b6972b2219a1cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db90bda9fb03c916c54eb29767d7d216",
+    "url": "/room4-task/index.html"
+  },
+  {
+    "revision": "dfaadbf8a8eb6926b463",
+    "url": "/room4-task/static/css/main.fd524a1b.chunk.css"
+  },
+  {
+    "revision": "11416c392aab9f36631d",
+    "url": "/room4-task/static/js/2.146f09a6.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/room4-task/static/js/2.146f09a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfaadbf8a8eb6926b463",
+    "url": "/room4-task/static/js/main.c86b0ef7.chunk.js"
+  },
+  {
+    "revision": "758c65d792d5150666a1",
+    "url": "/room4-task/static/js/runtime-main.ed56e5ec.js"
+  }
+]);
